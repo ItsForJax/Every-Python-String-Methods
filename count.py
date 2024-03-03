f4c7e8eff65719b9f53eb48720623c629447fb4b
4jax <dadelos.ddls@gmail.com>
2024-03-03 17:20:42 +0800

    count method

---
+++ count.py
@@ -0,0 +1,8 @@
+# All string methods showcase by Erickson Dela Soledad
+
+# Count - Returns the number of occurrences of substring `sub` in the string. Optional parameters `start` and `end` specify the slice of the string to search within. 
+
+string_input = "Hello World"
+print(string_input.count('l'))
+
+# prints -> 3 (since there are 3 occurences of the letter "l")
