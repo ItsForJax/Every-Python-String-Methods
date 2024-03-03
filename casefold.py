4d84f7112435f83439729b211c17476d92798b32
4jax <dadelos.ddls@gmail.com>
2024-03-03 17:18:28 +0800

    casefold method

---
+++ casefold.py
@@ -0,0 +1,8 @@
+# All string methods showcase by Erickson Dela Soledad
+
+# Casefold - Returns a casefolded copy of the string, suitable for caseless comparisons. 
+
+string_input = "Hello World"
+print(string_input.casefold())
+
+# prints -> hello world (all the letter are small)
