2257d758edaaf5960b0a574e020935b765b0919b
4jax <dadelos.ddls@gmail.com>
2024-03-03 17:26:29 +0800

    removed files

--- encode.py
+++
@@ -1,8 +0,0 @@
-# All string methods showcase by Erickson Dela Soledad
-
-# Encode - Returns an encoded version of the string using the specified encoding. 
-
-string_input = "Hello World"
-print(string_input.encode())
-
-# prints -> b'Hello World' (which is the utf-8 encoding version of the input string)
--- expandtabs.py
+++
@@ -1,8 +0,0 @@
-# All string methods showcase by Erickson Dela Soledad
-
-# Expandtabs - Returns a copy of the string where tab characters are replaced by one or more spaces, depending on the current column and the given tab size. 
-
-string_input = "\tHello\tWorld"
-print(string_input.expandtabs(10)) # default is 8 space
-
-# prints -> "          Hello     World" 
