ba9d2151d8f10df1e8f8da1f76fc94eee5dee3f9
4jax <dadelos.ddls@gmail.com>
2024-03-03 17:19:59 +0800

    format method

---
+++ format.py
@@ -0,0 +1,8 @@
+# All string methods showcase by Erickson Dela Soledad
+
+# Format - Formats the string with the supplied arguments and/or keyword arguments. 
+
+string_input = "Hello World {add}"
+print(string_input.format(add =  12)) 
+
+# prints -> Hello World 12 notice that the add inside {} was replaced using format
