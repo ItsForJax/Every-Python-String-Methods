731c2ee34a142bd3bd6f429d37a1538b0e30fb0b
4jax <dadelos.ddls@gmail.com>
2024-03-03 17:24:41 +0800

    deleted files

--- methods/casefold.py
+++
@@ -1,8 +0,0 @@
-# All string methods showcase by Erickson Dela Soledad
-
-# Casefold - Returns a casefolded copy of the string, suitable for caseless comparisons. 
-
-string_input = "Hello World"
-print(string_input.casefold())
-
-# prints -> hello world (all the letter are small)
